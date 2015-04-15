9722a51f728b29f58c35beecb1f47eefe7b0422d
kid <kid@LordDeathRay>
2015-04-15 14:43:07 -0400

    monte carlo is functioning correctly

--- monteCarlo.py
+++ monteCarlo.py
@@ -4,7 +4,8 @@ import math
 import time
 import random 
 
-
+xVal=[]
+yVal=[]
 def f(x):
 	return np.sin(x)
 
@@ -37,32 +38,36 @@ def trap(a,b,n):
 		x=x+h
 	return total*(h/2) 
 
+def monteCarlo(xRange,yRange,n):
+	global xVal,yVal
+	area=(xRange[0]+xRange[1])*(xRange[0]+xRange[1])
+	total=0
+	for r in range (n):
+		a=xRange[0]+xRange[1]*random.random()
+		b=yRange[0]+yRange[1]*random.random()
+		if b<f(a):
+			total+=1
+			xVal.append(a)
+			yVal.append(b)
+	return (total/n)*area
+
+
+xRange=[0,2]
+yRange=[0,2]
+startPoint=xRange[0]
+endPoint=xRange[1]
 st=time.time()
-x=[]
-y=[]
-startPoint=0
-endPoint=2
-for r in np.arange(startPoint,endPoint,.1):
-	x.append(r)
-	y.append(f(r))
-plt.plot(x,y,'-')
 
-xVal=[]
-yVal=[]
-total=0
-n=50000
-for r in range (n):
-	a=endPoint*random.random()
-	b=endPoint*random.random()
-	if b<f(a):
-		total+=1/n
-		xVal.append(a)
-		yVal.append(b)
-
-print("the monte carlo function result was ", total, " this result took " ,time.time()-st, " seconds")
+print("the monte carlo function result was ", monteCarlo(xRange,yRange,500000), " this result took " ,time.time()-st, " seconds")
 st=time.time()
 print("the trapazoid method result was  ",trap(startPoint,endPoint,2000) ," this result took " ,time.time()-st, " seconds")
 st=time.time()
 print("simpsons method produced the result " , integrate(startPoint,endPoint,80)," this result took " ,time.time()-st, " seconds")
-plt.plot(xVal,yVal,marker='o',linestyle='')
+x=[]
+y=[]
+for r in np.arange(startPoint,endPoint,.1):
+	x.append(r)
+	y.append(f(r))
+plt.plot(x,y,'-',color='blue')
+plt.plot(xVal,yVal,marker='o',linestyle='',color="green")
 plt.show()
